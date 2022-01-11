1974d9c004abba4c73936b57b2f7afc4d9357c61
cxl <2458083496@qq.com>
2022-01-11 13:21:26 +0800

    初始化

---
+++ src/utils/request.js
@@ -0,0 +1,50 @@
+import axios from 'axios'
+import store from '../store'
+import router from '../router/index.js'
+import {ElMessage} from "element-plus";
+
+const instance = axios.create({
+    baseURL: 'http://localhost:8989/gwhx/',
+    timeout:5000
+});
+
+// 添加请求拦截器
+instance.interceptors.request.use(function (config) {
+    // 在发送请求之前做些什么
+    config.headers["Content-Type"] = "application/json;charset=utf-8";
+    // if (config.url == 'users/login') {
+    //     return config;
+    // }
+    // if(localStorage.getItem("user") == null){
+    //     router.push('/login').then(r => {
+    //         ElMessage.error('登陆失效');
+    //     });
+    // }else {
+    //     config.headers["token"] = JSON.parse(localStorage.getItem("user")).user.userId;
+    // }
+    return config;
+}, function (error) {
+    // 对请求错误做些什么
+    return Promise.reject(error);
+});
+
+// 添加响应拦截器
+instance.interceptors.response.use(function (response) {
+    // 对响应数据做点什么
+    if(response.config.responseType == 'blob'){
+        return response.data;
+    }
+    return response;
+}, function (error) {
+    // 对响应错误做点什么
+    // router.push('/login').then(r => {
+    //
+    // });
+    ElMessage.error({
+        message: error.message,
+        duration: 1000
+    })
+    return Promise.reject(error);
+});
+
+export default instance;
